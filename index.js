18b2e9e9b474ce11229bac29ceaf12e4f4b010fb
Miguel Soriano <miguel.sor.88@gmail.com>
2023-08-17 18:10:04 +1000

    completed the lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(miles) {
   // Write your code here!
+  if (miles < 400) {
+    return "This one is on me!";
+  } else if (miles > 400 && miles < 2000) {
+    return "That will be twenty bucks.";
+  } else if (miles > 2000 && miles < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  if (tip === "generous") {
+    return "Thank you so much.";
+  } else if (tip === "not as generous") {
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
 }
